760471fa1c1536d124cf5c117dd05bba1564654b
BMAX <152480620+bigmax1018@users.noreply.github.com>
2024-03-01 06:06:30 +0900

    update bot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import telebot
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 import pyautogui
 import time
+import io
 
 # Telegram Bot Token
 TOKEN = '7042651707:AAFeKArSV9cufcmZD0ZXygha_9g6ZV6pV84'
@@ -24,6 +25,7 @@ def send_screenshot(screenshot):
     screenshot.save('screenshot.png')
     # Send the screenshot to the Telegram channel
     with open('screenshot.png', 'rb') as photo:
+        bot.send_message(CHANNEL_ID, "new post")
         bot.send_photo(CHANNEL_ID, photo)
 
 # Main function to continuously monitor for new posts and send screenshots
@@ -34,9 +36,12 @@ def main():
             # Capture screenshot
             screenshot = capture_screenshot()
             # Send screenshot to Telegram channel
-            send_screenshot(screenshot)
-            # Wait for some time before checking again
-            time.sleep(60)  # Adjust the time interval as needed
+            with open('screenshot.png', 'rb') as photo_re:
+                 screenshot_bytes = io.BytesIO()
+                 screenshot.save(screenshot_bytes, format='PNG')
+                 if screenshot_bytes.getvalue() != photo_re.read():
+                    send_screenshot(screenshot)
+            time.sleep(3)  # Adjust the time interval as needed
 
 # Entry point of the script
 if __name__ == '__main__':
--- main_re.py
+++ main_re.py
@@ -1,13 +1,14 @@
 import telebot
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 import pyautogui
 import time
+import io
 
 # Telegram Bot Token
-TOKEN = '7185979392:AAFNf7WyeiCTR5JaSTX07r5UrwWVbksEzbU'
+TOKEN = '7042651707:AAFeKArSV9cufcmZD0ZXygha_9g6ZV6pV84'
 
 # Channel ID where you want to send screenshots
-CHANNEL_ID = '@foreardtester'
+CHANNEL_ID = '@my_test_screenshot'
 
 # Initialize the bot
 bot = telebot.TeleBot(TOKEN)
@@ -15,7 +16,8 @@ bot = telebot.TeleBot(TOKEN)
 # Function to capture screenshot
 def capture_screenshot():
     # Use appropriate method to capture screenshot in Android
-    screenshot = pyautogui.screenshot(region=(620, 100, 540, 900))  # Assuming you are running this script on a Windows machine
+    # screenshot = pyautogui.screenshot(region=(620, 100, 540, 900))  # Assuming you are running this script on a Windows machine
+    screenshot = pyautogui.screenshot(region=(25, 100, 540, 930))
     return screenshot
 
 # Function to send screenshot to Telegram channel
@@ -29,14 +31,21 @@ def send_screenshot(screenshot):
 # Main function to continuously monitor for new posts and send screenshots
 def main():
     while True:
-        # Check for new posts in DatChat (You need to implement this part)
+        # Check for new posts in 
+        # DatChat (You need to implement this part)
         # if new_post_detected():
             # Capture screenshot
             screenshot = capture_screenshot()
             # Send screenshot to Telegram channel
-            send_screenshot(screenshot)
+            with open('screenshot_re.png', 'rb') as photo_re:
+                 screenshot_bytes = io.BytesIO()
+                 screenshot.save(screenshot_bytes, format='PNG')
+                 if screenshot_bytes.getvalue() != photo_re.read():
+                    send_screenshot(screenshot)
+                 else:
+                    print("Screenshots are the same.")
             # Wait for some time before checking again
-            time.sleep(60)  # Adjust the time interval as needed
+            # time.sleep(1)  # Adjust the time interval as needed
 
 # Entry point of the script
 if __name__ == '__main__':
