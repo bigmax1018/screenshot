ed4253b4289ff1d90d0822d91b1227414cde2a3e
BMAX <152480620+bigmax1018@users.noreply.github.com>
2024-03-06 16:47:53 +0900

    update project

--- user_check.py
+++ user_check.py
@@ -27,35 +27,43 @@ with open("channel_list.txt", "r") as file:
         channel_list = file.readlines()
 print(channel_list)
 # Function to kick user from specified channels
-async def handle_special_channel_kick(update: Update, context: CallbackContext) -> None:
+async def handle_special_channel_kick(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Check if the kick happened in the special channel
-    await update.message.reply_text("Kick!")
+    await update.message.reply_text(update.effective_chat.id)
     if update.effective_chat.id in channel_list:
         kicked_user_id = update.effective_user.id
-        print(kicked_user_id + 'kicked')
         # Iterate through other special channels and kick the user
         for channel_id in channel_list:
-            bot.kick_chat_member(channel_id, kicked_user_id)
+            await context.bot.ban_chat_member(channel_id, kicked_user_id)
         # Add the user to the kick member list
         with open("kick_member_list.txt", "a") as file:
             file.write(str(kicked_user_id) + "\n")
 
 # Function to reject user when kicked someone rejoin
-async def reject_rejoined_users(update: Update, context: CallbackContext) -> None:
+async def reject_rejoined_users(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     rejoined_user_id = update.effective_user.id
     await update.message.reply_text("Request Join!" + rejoined_user_id)
     # Check if the rejoined user is in the kick member list
     with open("kick_member_list.txt", "r") as file:
         kick_members = file.readlines()
         if str(rejoined_user_id) + "\n" in kick_members:
-            bot.kick_chat_member(update.effective_chat.id, rejoined_user_id)
+            # bot.kick_chat_member(update.effective_chat.id, rejoined_user_id)
+            await context.bot.ban_chat_member(update.effective_chat.id, rejoined_user_id)
+            
 
 async def error(update: Update, context: CallbackContext):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 async def nice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    with open('screenshot.png', 'rb') as photo:
-        await update.message.reply_photo(photo)
+    channel_id = update.message.chat_id
+    # user = context.bot.get_chat('@thaumaturge_1027')
+    # if user:
+    #     await context.bot.ban_chat_member(channel_id, user.id)
+    #     with open('screenshot.png', 'rb') as photo:
+    #         await update.message.reply_text(channel_id)
+    #         await update.message.reply_photo(photo)
+    # else:
+    #     await update.message.reply_text('User not found') 
 
 def main():
     application = Application.builder().token("7039684234:AAGPI_DnYMCZpePpAV16UHPMbUOGuDlG5O0").build()
@@ -63,9 +71,7 @@ def main():
     application.add_handler(ChatMemberHandler(handle_special_channel_kick, ChatMemberHandler.MY_CHAT_MEMBER))
     application.add_handler(ChatJoinRequestHandler(reject_rejoined_users))
     application.add_handler(CommandHandler('nice', nice))
-    # application.run_polling()
-    while True:
-         a = "aaa"
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
